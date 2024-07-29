96be50a8d7aa308745d8be93bcb65761ec56a7db
Dmitri Yourchev <d.yurchev@yandex.ru>
2024-07-29 21:28:42 +0600

    feat: parallax first approach

--- src/components/CharCard.jsx
+++ src/components/CharCard.jsx
@@ -1,24 +1,10 @@
 import "./CharCard.css";
 
 const CharCard = (char) => {
-  let charImg;
-  let charName;
-
-  switch (char.name) {
-    case "wolf":
-      charName = "Серый Шторм";
-      charImg = "/wolf_char.png";
-      break;
-    case "lion":
-      charName = "Лев Риф";
-      charImg = "/lion_char.png";
-      break;
-  }
 
   return (
     <div id={"charcard"}>
-      <div id={"charname"}>{charName}</div>
-      <img src={charImg} ></img>
+      <img src={char.charImg} ></img>
     </div>
   );
 };
--- src/components/CharsGallery.jsx
+++ src/components/CharsGallery.jsx
@@ -1,36 +1,103 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCreative } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Parallax, Pagination, Navigation } from "swiper/modules";
 import CharCard from "./CharCard";
+
+const Characters = [
+  {
+    name: 'Руна Лавинка',
+    img: 'fox_char.png',
+    trials: '1 попытка',
+    cost: 1,
+    trophies: 623,
+    jackpot: 10000,
+    theme: 'orange'
+  },
+  {
+    name: 'Серый Шторм',
+    img: 'wolf_char.png',
+    trials: '3 попытки',
+    cost: 3,
+    trophies: 891,
+    jackpot: 25000,
+    theme: 'violet'
+  },
+  {
+    name: 'Лев Риф',
+    img: 'fox_char.png',
+    trials: '5 попыток',
+    cost: 5,
+    trophies: 1085,
+    jackpot: 50000,
+    theme: 'red'
+  },
+  {
+    name: 'Громилаг',
+    img: 'bear_char.png',
+    trials: '10 попыток',
+    cost: 10,
+    trophies: 1385,
+    jackpot: 100000,
+    theme: 'red'
+  }
+];
 
 const CharsGallery = () => (
   <Swiper
-    grabCursor={true}
-    effect={"creative"}
-    creativeEffect={{
-      prev: {
-        shadow: true,
-        translate: [0, 0, -400],
-      },
-      next: {
-        translate: ["100%", 0, 0],
-      },
+    style={{
+      "--swiper-navigation-color": "#fff",
+      "--swiper-pagination-color": "#fff",
     }}
-    modules={[EffectCreative]}
+    speed={600}
+    parallax={true}
+    navigation={true}
+    modules={[Parallax, Pagination, Navigation]}
     className="mySwiper"
   >
-    <SwiperSlide className={'card-background pirate-bay-at-night'}>
-      <CharCard name={"wolf"} background={"/pirate_theme_background.webp"} />
-    </SwiperSlide>
-    <SwiperSlide  className={'card-background burning-deck'}>
-      <CharCard name={"lion"} />
-    </SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-    <SwiperSlide>Slide 5</SwiperSlide>
-    <SwiperSlide>Slide 6</SwiperSlide>
-    <SwiperSlide>Slide 7</SwiperSlide>
-    <SwiperSlide>Slide 8</SwiperSlide>
-    <SwiperSlide>Slide 9</SwiperSlide>
+    <div
+      slot="container-start"
+      className="parallax-bg"
+      style={{
+        backgroundImage: "url(/scifi_background.webp)",
+      }}
+      data-swiper-parallax="-20%"
+    >
+      <div className="bird-container bird-container--one">
+        <div className="bird bird--one"></div>
+      </div>
+    </div>
+    {Characters.map((character, index) => (
+      <SwiperSlide key={index}>
+        <div
+          className={`title underdog-regular char-name ${character.theme}`}
+          data-swiper-parallax="-600"
+        >
+          {character.name}
+        </div>
+        <div
+          className={`subtitle underdog-regular char-stats ${character.theme}`}
+        >
+          <div className="char-stat-line" data-swiper-parallax="-400">Добыча</div>
+          <div data-swiper-parallax="-400">{character.trophies} TON</div>
+          <div className="char-stat-line" data-swiper-parallax="-250">Большой трофей</div>
+          <div data-swiper-parallax="-250">{character.jackpot} TON</div>
+        </div>
+        <div
+          className="subtitle underdog-regular text-shadow text-overlay char-contract chalk-board"
+          data-swiper-parallax="-300"
+        >
+          <div>КОНТРАКТ:</div>
+          <br />
+          <div>{character.cost} TON</div>
+          <div>{character.trials}</div>
+        </div>
+        <div data-swiper-parallax="-100">
+          <CharCard charImg={`/${character.img}`} />
+        </div>
+      </SwiperSlide>
+    ))}
   </Swiper>
 );
 
